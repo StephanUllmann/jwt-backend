872ea8d40d95b5119b91a7ea351c1eef4ea4d3da
Stephan Ullmann <stephan.ullmann@posteo.de>
2024-06-14 10:27:59 +0200

    added mock post routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectDB from './connectDB.js';
-
+import postRouter from './routes/posts.js';
 import userRouter from './routes/user.js';
 
 const app = express();
@@ -8,6 +8,7 @@ app.use(express.json());
 connectDB();
 
 app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.listen(8080, () => {
   console.log('server is listening on port 8080');
---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import requireAuth from '../requireAuth.js';
+const postRouter = express.Router();
+
+const posts = [{ id: 0, title: 'Awesome Auth', body: 'Authentication and Authorization are awesome' }];
+
+postRouter.get('/', (req, res) => res.json(posts));
+postRouter.post('/', (req, res) => {
+  const id = posts.length;
+  const newPost = req.body;
+  posts.push({ id, ...newPost });
+  res.json(posts);
+});
+
+export default postRouter;
